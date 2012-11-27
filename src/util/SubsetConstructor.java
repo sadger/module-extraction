58de5ede616192b9c76e8b9bd360ba8f95fd935e
wgatens <wgatens@localhost>
2012-11-27 23:25:19 +0000

    Completed subset generating for signatures, needs refactoring however.

--- src/util/SubsetConstructor.java
+++ src/util/SubsetConstructor.java
@@ -7,6 +7,8 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import javax.security.auth.kerberos.KerberosKey;
+
 import loader.OntologyLoader;
 
 import org.semanticweb.owlapi.model.OWLClass;
@@ -21,11 +23,13 @@ import checkers.DefinitorialDependencies;
 
 public class SubsetConstructor {
 	private SignatureAnalyser sigAnalyser;
-	private static final int MAX_PAIR_SIZE = 10000; 
+	private static final int MAX_PAIR_SIZE = 15000; 
 	
+	DefinitorialDependencies deps;
 	
 	public SubsetConstructor(Set<OWLLogicalAxiom> axioms) {
 		sigAnalyser = new SignatureAnalyser(axioms);
+		deps = new DefinitorialDependencies(axioms);
 	}
 
 	/*
@@ -34,16 +38,17 @@ public class SubsetConstructor {
 	 */
 	public void generateSets(Map<Integer, Set<OWLClass>> numberMap, int averageSize, int setSize){
 		
-		//We want each pair to add up the average so 
-		//The sum of each pair needs to be twice average size
-		//Also the maximum values of each pair can be that of twice average
-		//size i.e if x=averageSize*2 and y=0 then the x+y/2=averageSize
+		
+		/* We want each pair to add up the average so 
+		The sum of each pair needs to be twice average size
+		Also the maximum values of each pair can be that of twice average
+		size i.e if x=averageSize*2 and y=0 then the x+y/2=averageSize */
 		
 		int checkSize = averageSize * 2;
 		
 		Set<OWLClass> signature = new HashSet<OWLClass>();
 		Set<Pair<OWLClass, OWLClass>> averageSizePairs = new HashSet<Pair<OWLClass,OWLClass>>();
-
+		HashSet<OWLClass> uniqueClasses = new HashSet<OWLClass>();
 		
 		for(int x : numberMap.keySet()){
 			if(x <= checkSize){
@@ -54,8 +59,9 @@ public class SubsetConstructor {
 								for(OWLClass cls2 : numberMap.get(y)){
 									if(!cls1.equals(cls2) && averageSizePairs.size() < MAX_PAIR_SIZE){
 										averageSizePairs.add(new Pair<OWLClass, OWLClass>(cls1, cls2));
+										System.out.println(x + ":" + y);
+										uniqueClasses.add(cls2);	
 									}
-
 								}
 							}
 
@@ -70,8 +76,10 @@ public class SubsetConstructor {
 		averageSizePairs.clear();
 		Collections.shuffle(pairList);
 		
-
-		while(signature.size() != setSize){
+		
+		int desiredSize = uniqueClasses.size() > setSize ? setSize : uniqueClasses.size()*2;  
+		
+		while(signature.size() != desiredSize){
 			for(Pair<OWLClass,OWLClass> p : pairList){
 				OWLClass first = p.getFirst();
 				OWLClass second = p.getSecond();
@@ -85,21 +93,23 @@ public class SubsetConstructor {
 		
 		System.out.println(signature.size());
 		System.out.println(sigAnalyser.averageDependencySize(signature));
-		System.out.println(signature);
+		ArrayList<OWLClass> sigList = new ArrayList<OWLClass>(signature);
+		Collections.sort(sigList);
+		System.out.println(sigList);
 		
+
+
 		
 	}
+	
 
 
 
 	public static void main(String[] args) {
-		OWLOntology ont = OntologyLoader.loadOntology(ModulePaths.getOntologyLocation() + "NCI/nci-08.09d-terminology.owl");
+		OWLOntology ont = OntologyLoader.loadOntology(ModulePaths.getOntologyLocation() + "NCI/expr/nci-08.09d-terminology.owl");
 		SubsetConstructor constructor = new SubsetConstructor(ont.getLogicalAxioms());
 		DefinitorialDependencies deps = new DefinitorialDependencies(ont.getLogicalAxioms());
-		
-		for (int i = 0; i < 5 ; i++) {
-			constructor.generateSets(deps.getDependenciesByNumber(),0, 10);
-		}
+
 	}
 
 
