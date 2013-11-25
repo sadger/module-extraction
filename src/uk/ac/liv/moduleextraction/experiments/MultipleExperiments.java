5d81e0af31ca05fed5a428b98969231ae9e1227b
wgatens <wgatens@localhost>
2013-11-25 15:18:30 +0000

    Method to copy directory structure for signature experiments to results directory.

--- src/uk/ac/liv/moduleextraction/experiments/MultipleExperiments.java
+++ src/uk/ac/liv/moduleextraction/experiments/MultipleExperiments.java
@@ -2,8 +2,10 @@ package uk.ac.liv.moduleextraction.experiments;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Set;
+import java.util.Stack;
 
 import org.semanticweb.owlapi.model.OWLEntity;
 import org.semanticweb.owlapi.model.OWLOntology;
@@ -16,74 +18,95 @@ import uk.ac.liv.moduleextraction.signature.SigManager;
 import uk.ac.liv.moduleextraction.signature.SignatureGenerator;
 import uk.ac.liv.ontologyutils.loader.OntologyLoader;
 import uk.ac.liv.ontologyutils.util.ModulePaths;
+import uk.ac.liv.ontologyutils.util.ModuleUtils;
 
 public class MultipleExperiments {
-	
-	
-	
+
+
+
 	public void runExperiments(OWLOntology ontology, File signaturesLocation, Experiment experimentType) throws IOException{
 		Experiment experiment = experimentType;
 		SigManager sigManager = new SigManager(signaturesLocation);
 		File[] files = signaturesLocation.listFiles();
 		Arrays.sort(files); 
-		
+
 		/* Create new folder in result location with same name as signature
 		folder */
-		File newResultFolder = new File(ModulePaths.getResultLocation() + "/sharednames/" + signaturesLocation.getName());
-		if(!newResultFolder.exists()){
-			System.out.println("Making new directory " + newResultFolder.getAbsolutePath());
-			newResultFolder.mkdir();
-		}
-		
+		File newResultFolder = copyDirectoryStructure(signaturesLocation, "Signatures",new File(ModulePaths.getResultLocation()));
+
+
 		int experimentCount = 1;
 		for(File f : files){
 			if(f.isFile()){
-				
+
 				System.out.println("Experment " + experimentCount + ": " + f.getName());
-			
+
 				Set<OWLEntity> sig = sigManager.readFile(f.getName());
 				experiment.performExperiment(sig);
-				
+
 				//New folder in result location - same name as sig file
 				File experimentLocation = new File(newResultFolder.getAbsoluteFile() + "/" + f.getName());
 				if(!experimentLocation.exists()){
 					experimentLocation.mkdir();
 				}
-				
+
 				//Save the signature with the experiment
 				SigManager managerWriter = new SigManager(experimentLocation);
 				managerWriter.writeFile(sig, "signature");
-				
+
 				//Write any metrics
 				experiment.writeMetrics(experimentLocation);
 				experimentCount++;
 			}
 		}
 	}
-	
-	public static void main(String[] args) throws OWLOntologyCreationException, NotEquivalentToTerminologyException, IOException, OWLOntologyStorageException {
-	
-//		File sharedNameDirectory = new File(ModulePaths.getOntologyLocation() + "/SharedConceptNames/");
-//		for(File f : sharedNameDirectory.listFiles()){
-//			if(f.isFile()){
-//				OWLOntology ont = OntologyLoader.loadOntologyAllAxioms(f.getAbsolutePath());
-//				System.out.println(f.getName());
-//				new MultipleExperiments().runExperiments(ont, new File(ModulePaths.getSignatureLocation() + "/sharednames/" + f.getName()), new SharedNameExperiment(ont));
-//			}
-//		}
-//		
 
-		OWLOntology ontology = OntologyLoader.loadOntologyAllAxioms(ModulePaths.getOntologyLocation() + "/SharedConceptNames/CHEBI");
-		
-		SignatureGenerator gen = new SignatureGenerator(ontology.getLogicalAxioms());
-		for (int i = 1; i <= 1000; i++) {
-			System.out.println("Experiment " + i + ":");
-			new SharedNameExperiment(ontology).performExperiment(gen.generateRandomSignature(500));
+	/**
+	 * Copy the structure of a source directory to another location creating a directory
+	 * for each directory in the path 
+	 * @param source - The directory to begin copying from 
+	 * @param sourceLimit - Only start copying the source from this directory 
+	 * @param destination - The top level to copy the structure too
+	 * @return File - path of deepest part of new directory structure.
+	 * Example: copyDirectoryStructure(//a/x/y/z/,"x", /home/) 
+	 * result File /home/y/z/  
+	 */
+	public File copyDirectoryStructure(File source, String sourceLimit, File destination) {
+		Stack<String> directoriesToWrite = new Stack<String>();
+
+		//Push all the directories from the end backwards to the sourceLimit (if applicable)
+		while(!source.getName().equals(sourceLimit) || source.getParent() == null){
+			if(source.isDirectory()){
+				directoriesToWrite.push(source.getName());	
+			}
+			source = source.getParentFile();
 		}
 
+		//Build the path from the start of the destinated using the pushed directory names2
+		String target = destination.getAbsolutePath();
+		while(!directoriesToWrite.isEmpty()){
+			target = target + "/" + directoriesToWrite.pop();
+		}
+
+		File targetFile = new File(target);
+
+		if(!targetFile.exists()){
+			System.out.println("Making directory: " + target);
+			targetFile.mkdirs();
+		}
+
+		return targetFile;
+	} 
+
+	public static void main(String[] args) throws OWLOntologyCreationException, NotEquivalentToTerminologyException, IOException, OWLOntologyStorageException {
+
+		OWLOntology ont = OntologyLoader.loadOntologyAllAxioms(ModulePaths.getOntologyLocation() + "SharedConceptNames/vivo");
+		File sigDirectory = new File(ModulePaths.getSignatureLocation() + "testingnames/vivo");
 		
+		new MultipleExperiments().runExperiments(ont, new File(ModulePaths.getSignatureLocation() + "/testingnames/vivo"),new SharedNameExperiment(ont));
+
 	}
-	
-	
-	
+
+
+
 }
