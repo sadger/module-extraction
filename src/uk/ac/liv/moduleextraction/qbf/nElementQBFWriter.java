27b21041ba3c8eb5504bc90eeb6165aae5bc3827
westforduk <westforduk@gmail.com>
2014-11-17 21:24:30 +0000

    Changed n-element qbf writer for new tseitin transformation

--- src/uk/ac/liv/moduleextraction/qbf/nElementQBFWriter.java
+++ src/uk/ac/liv/moduleextraction/qbf/nElementQBFWriter.java
@@ -9,15 +9,14 @@ import uk.ac.liv.ontologyutils.loader.OntologyLoader;
 import uk.ac.liv.ontologyutils.util.ModulePaths;
 import uk.ac.liv.ontologyutils.util.ModuleUtils;
 import uk.ac.liv.propositional.formula.PropositionalFormula;
-import uk.ac.liv.propositional.nSeparability.nAxiomToSATMapping;
+import uk.ac.liv.propositional.nSeparability.ClauseStore;
+import uk.ac.liv.propositional.nSeparability.nAxiomToClauseStore;
 import uk.ac.liv.propositional.nSeparability.nEntityConvertor;
-import uk.ac.liv.propositional.satclauses.SATSet;
 
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.ExecutionException;
@@ -33,13 +32,17 @@ public class nElementQBFWriter {
     private final HashSet<OWLEntity> signature;
     private final HashSet<OWLEntity> classesNotInSignature;
     private final int DOMAIN_SIZE;
-    private static LoadingCache<Integer,nAxiomToSATMapping> convertors;
-    private nAxiomToSATMapping mapper;
-    private ArrayList<SATSet> clauses;
+    private static LoadingCache<Integer,nAxiomToClauseStore> convertors;
+    private nAxiomToClauseStore mapper;
+
+    private HashSet<int[]> clauses;
     private HashSet<Integer> variables;
+    private HashSet<Integer> freshVariables;
     private nEntityConvertor entityUnderAllInterpreations;
 
-    public nElementQBFWriter(int domainSize, Set<OWLLogicalAxiom> ontology, Set<OWLEntity> signatureAndSigM) throws IOException {
+    private boolean isUnsatisfiable = false;
+
+    public nElementQBFWriter(int domainSize, Set<OWLLogicalAxiom> ontology, Set<OWLEntity> signatureAndSigM) throws IOException, ExecutionException {
         this.DOMAIN_SIZE = domainSize;
         File directoryToWrite = new File("/tmp/");
         this.qbfFile = File.createTempFile("qbf", ".qdimacs",directoryToWrite);
@@ -47,24 +50,21 @@ public class nElementQBFWriter {
         this.signature = new HashSet<OWLEntity>(signatureAndSigM);
         this.classesNotInSignature = new HashSet<OWLEntity>();
         this.entityUnderAllInterpreations = new nEntityConvertor(domainSize);
-        this.clauses = new ArrayList<SATSet>();
+        this.clauses = new HashSet<int[]>();
         this.variables = new HashSet<Integer>();
+        this.freshVariables = new HashSet<Integer>();
         if (convertors == null){
             convertors = CacheBuilder.newBuilder().
-                    build(new CacheLoader<Integer, nAxiomToSATMapping>() {
+                    build(new CacheLoader<Integer, nAxiomToClauseStore>() {
                         @Override
-                        public nAxiomToSATMapping load(Integer i){
-                            return new nAxiomToSATMapping(i);
+                        public nAxiomToClauseStore load(Integer i){
+                            return new nAxiomToClauseStore(i);
                         }
                     });
         }
-        try {
-            this.mapper = convertors.get(DOMAIN_SIZE);
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
 
-        collectAndCountQBFClauses();
+        this.mapper = convertors.get(DOMAIN_SIZE);
+        collectClausesAndVariables();
         populateSignatures();
         generateQBFProblem();
     }
@@ -82,12 +82,30 @@ public class nElementQBFWriter {
         classesNotInSignature.remove(factory.getOWLNothing());
     }
 
-    private void collectAndCountQBFClauses() {
+    private boolean isUnsatisfiable(){
+        return isUnsatisfiable;
+    }
+
+    private boolean convertedClausesAreEmpty(){
+        return clauses.isEmpty();
+    }
+
+    private void collectClausesAndVariables() {
         for(OWLLogicalAxiom axiom: ontology){
-            ArrayList<SATSet> converted = mapper.convertAxiom(axiom);;
-            for(SATSet clause : converted){
-                variables.addAll(clause.getVariables());
-                clauses.add(clause);
+            ClauseStore clauseStore = mapper.convertAxiom(axiom);
+            if(clauseStore.hasConstantValue()){
+                boolean constantValue = clauseStore.getConstantValue();
+                if(constantValue == false){
+                    isUnsatisfiable = true;
+                    break;
+                }
+                //Skip constant "true" clauses
+            }
+            else{
+                clauses.addAll(clauseStore.getClauses());
+                variables.addAll(clauseStore.getVariables());
+                freshVariables.addAll(clauseStore.getFreshVariables());
+                System.out.println(clauseStore);
             }
         }
     }
@@ -140,12 +158,10 @@ public class nElementQBFWriter {
     private void writeUniversalQuantifiers(BufferedWriter writer) throws IOException{
         if(!signature.isEmpty()) {
             writer.write("a ");
-            for (OWLEntity ent : signature) {
-                for (PropositionalFormula underEach : ent.accept(entityUnderAllInterpreations)) {
-                    Integer mapping = mapper.lookupMapping(underEach);
-                    if (mapping != null) {
-                        writer.write(mapping + " ");
-                    }
+            for(OWLEntity sigEnt : signature){
+                for(PropositionalFormula ent : sigEnt.accept(entityUnderAllInterpreations)){
+                    int entValue = mapper.lookupMapping(ent);
+                    writer.write(entValue + " ");
                 }
             }
             writer.write("0");
@@ -154,15 +170,16 @@ public class nElementQBFWriter {
     }
 
     private void writeExistentialQuantifiers(BufferedWriter writer) throws IOException{
-        if(!classesNotInSignature.isEmpty()){
+        if(!classesNotInSignature.isEmpty() || !freshVariables.isEmpty()){
             writer.write("e ");
-            for(OWLEntity ent : classesNotInSignature){
-                    for (PropositionalFormula underEach : ent.accept(entityUnderAllInterpreations)) {
-                        Integer mapping = mapper.lookupMapping(underEach);
-                        if (mapping != null) {
-                            writer.write(mapping + " ");
-                        }
-                    }
+            for(OWLEntity sigEnt : classesNotInSignature){
+                for(PropositionalFormula ent : sigEnt.accept(entityUnderAllInterpreations)){
+                    int entValue = mapper.lookupMapping(ent);
+                    writer.write(entValue + " ");
+                }
+            }
+            for(Integer fresh : freshVariables){
+                writer.write(fresh + " ");
             }
             writer.write("0");
             writer.newLine();
@@ -170,9 +187,9 @@ public class nElementQBFWriter {
     }
 
     private void writeClauses(BufferedWriter writer) throws IOException{
-        for(SATSet s : clauses){
-            for(int i : s){
-                writer.write(i  + " ");
+        for(int[] clause : clauses){
+            for(int var : clause){
+                writer.write(var + " ");
             }
             writer.write("0");
             writer.newLine();
@@ -182,21 +199,30 @@ public class nElementQBFWriter {
     
 
     public static void main(String[] args) {
-        OWLOntology ont = OntologyLoader.loadOntologyAllAxioms(ModulePaths.getOntologyLocation() + "/NCI/Profile/NCI-star.owl");
-        Set<OWLLogicalAxiom> subset = ModuleUtils.generateRandomAxioms(ont.getLogicalAxioms(), 500);
+        OWLOntology ont = OntologyLoader.loadOntologyAllAxioms(ModulePaths.getOntologyLocation() + "/examples/simple.krss");
+
         System.out.println("LOADED");
+
         OWLDataFactory f = ont.getOWLOntologyManager().getOWLDataFactory();
-//        ModuleUtils.remapIRIs(ont,"X");
+        ModuleUtils.remapIRIs(ont,"X");
+        for(OWLLogicalAxiom ax : ont.getLogicalAxioms()){
+            System.out.println(ax);
+        }
+
         HashSet<OWLEntity> signature = new HashSet<OWLEntity>();
         OWLClass a = f.getOWLClass(IRI.create("X#A"));
         OWLClass b = f.getOWLClass(IRI.create("X#B"));
         OWLClass c = f.getOWLClass(IRI.create("X#C"));
         OWLObjectProperty r = f.getOWLObjectProperty(IRI.create("X#r"));
         signature.add(a);
+        signature.add(b);
         signature.add(r);
+
         try {
-            new nElementQBFWriter(2,subset,signature);
+            new nElementQBFWriter(1,ont.getLogicalAxioms(),signature);
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
             e.printStackTrace();
         }
     }
