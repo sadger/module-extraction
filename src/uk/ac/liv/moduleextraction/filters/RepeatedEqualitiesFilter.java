125850ae349f93c498f7b94c113b8978e1342b1c
William Gatens W Gatens <William Gatens W.Gatens@liverpool.ac.uk>
2015-05-27 15:20:23 +0100

    Moved filter to own package

---
+++ src/uk/ac/liv/moduleextraction/filters/RepeatedEqualitiesFilter.java
@@ -0,0 +1,38 @@
+package uk.ac.liv.moduleextraction.filters;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLLogicalAxiom;
+
+import uk.ac.liv.moduleextraction.filters.SupportedFilter;
+import uk.ac.liv.ontologyutils.axioms.AxiomStructureInspector;
+
+public class RepeatedEqualitiesFilter implements SupportedFilter {
+	
+	private AxiomStructureInspector inspector;
+	public RepeatedEqualitiesFilter(AxiomStructureInspector inspector) {
+		this.inspector = inspector;
+	}
+
+	@Override
+	public boolean isRequired() {
+		return inspector.countNamesWithRepeatedEqualities() > 0;
+	}
+
+	@Override
+	public Set<OWLLogicalAxiom> getUnsupportedAxioms(Collection<OWLLogicalAxiom> axioms) {
+		Set<OWLLogicalAxiom> unsupported = new HashSet<OWLLogicalAxiom>();
+		for(OWLClass repeated : inspector.getNamesWithRepeatedEqualities()){
+			Set<OWLLogicalAxiom> repeatedEqualities = new HashSet<OWLLogicalAxiom>(inspector.getDefinitions(repeated));
+			OWLLogicalAxiom toKeep = repeatedEqualities.toArray(new OWLLogicalAxiom[repeatedEqualities.size()])[0];
+			// Keep an axiom and the rest are unsupported (no longer any repeated equality after their removal) 
+			repeatedEqualities.remove(toKeep);
+			unsupported.addAll(repeatedEqualities);
+		}
+		return unsupported;
+	}
+
+}
