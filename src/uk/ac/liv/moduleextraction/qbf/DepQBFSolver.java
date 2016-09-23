f82276fccb22592058e314ba2d92db5ea5a4f020
westforduk <westforduk@gmail.com>
2016-09-23 17:58:41 +0100

    Removed tests and unused classes

--- src/uk/ac/liv/moduleextraction/qbf/DepQBFSolver.java
+++ src/uk/ac/liv/moduleextraction/qbf/DepQBFSolver.java
@@ -2,7 +2,6 @@ package uk.ac.liv.moduleextraction.qbf;
 
 
 import com.google.common.base.Stopwatch;
-import depqbf4j.DepQBF4J;
 import org.semanticweb.owlapi.model.OWLEntity;
 import org.semanticweb.owlapi.model.OWLLogicalAxiom;
 import org.semanticweb.owlapi.model.OWLOntology;
@@ -107,67 +106,6 @@ public class DepQBFSolver {
     public boolean isSatisfiable() throws QBFSolverException, IOException {
         QBFSolver solver = new QBFSolver();
         return solver.isSatisfiable(qbfFile);
-    }
-
-    public boolean DEPQBFisSatisfiable() throws QBFSolverException, IOException {
-        boolean returnCode;
-        byte exitCode  = DepQBF4J.sat();
-        switch (exitCode){
-            case DepQBF4J.RESULT_SAT:
-                returnCode = true;
-                break;
-            case DepQBF4J.RESULT_UNSAT:
-                returnCode = false;
-                break;
-            default:
-                if(exitCode == DepQBF4J.RESULT_UNKNOWN){
-                    System.err.println("The result is unknown");
-                }
-                else{
-                    System.err.println("There was an error with the QBF solver, EXIT CODE " + exitCode);
-                }
-                File qbf = File.createTempFile("qbf",".qdimacs",new File("/tmp/"));
-                DepQBF4J.printToFile(qbf.getAbsolutePath());
-                System.err.println("QBF file dumped to: " + qbf.getAbsolutePath());
-                throw new QBFSolverException();
-
-        }
-        return returnCode;
-
-    }
-
-    private void DEPQBFconstructQBFProblem(){
-        int nestingLevel = 1;
-        DepQBF4J.create();
-        DepQBF4J.configure("--dep-man=simple");
-
-        if(!universal.isEmpty()){
-            DepQBF4J.newScopeAtNesting(DepQBF4J.QTYPE_FORALL, nestingLevel++);
-            for(int forall : universal){
-                DepQBF4J.add(forall);
-            }
-            DepQBF4J.add(0);
-        }
-
-        if(!existential.isEmpty()){
-            DepQBF4J.newScopeAtNesting(DepQBF4J.QTYPE_EXISTS, nestingLevel++);
-            for(int exists : existential){
-                DepQBF4J.add(exists);
-            }
-            DepQBF4J.add(0);
-        }
-
-        for(int[] clause : clauses){
-            for(int val : clause){
-                DepQBF4J.add(val);
-            }
-            DepQBF4J.add(0);
-        }
-    }
-
-
-    public void delete(){
-        DepQBF4J.delete();
     }
 
     public static void main(String[] args) {
