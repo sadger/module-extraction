36a0cd762122d47cc1be20a905510cd08a91a2b4
westforduk <westforduk@gmail.com>
2016-09-27 20:15:23 +0100

    More removal

--- src/uk/ac/liv/moduleextraction/checkers/ChainAxiomCollector.java
+++ src/uk/ac/liv/moduleextraction/checkers/ChainAxiomCollector.java
@@ -1,14 +1,12 @@
 package uk.ac.liv.moduleextraction.checkers;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import org.semanticweb.owlapi.model.OWLEntity;
 import org.semanticweb.owlapi.model.OWLLogicalAxiom;
-
 import uk.ac.liv.moduleextraction.chaindependencies.AxiomDependencies;
-import uk.ac.liv.moduleextraction.chaindependencies.ChainDependencies;
 import uk.ac.liv.moduleextraction.storage.DefinitorialAxiomStore;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class ChainAxiomCollector {
 
--- src/uk/ac/liv/moduleextraction/checkers/SyntacticDependencyChecker.java
+++ src/uk/ac/liv/moduleextraction/checkers/SyntacticDependencyChecker.java
@@ -1,15 +1,13 @@
 package uk.ac.liv.moduleextraction.checkers;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLEntity;
 import org.semanticweb.owlapi.model.OWLLogicalAxiom;
-
 import uk.ac.liv.moduleextraction.chaindependencies.AxiomDependencies;
-import uk.ac.liv.moduleextraction.chaindependencies.ChainDependencies;
 import uk.ac.liv.ontologyutils.axioms.AxiomSplitter;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class SyntacticDependencyChecker {
 	
