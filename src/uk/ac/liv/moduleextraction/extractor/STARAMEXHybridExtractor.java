b757a423e9148db7e616ae4e38b81b5f9c4263b4
westforduk <westforduk@gmail.com>
2016-09-30 18:58:34 +0100

    Completed STAR-AMEX hybrid with generic extractor - needs testing

--- src/uk/ac/liv/moduleextraction/extractor/STARAMEXHybridExtractor.java
+++ src/uk/ac/liv/moduleextraction/extractor/STARAMEXHybridExtractor.java
@@ -7,6 +7,7 @@ import uk.ac.liv.moduleextraction.filters.RepeatedEqualitiesFilter;
 import uk.ac.liv.moduleextraction.filters.SharedNameFilter;
 import uk.ac.liv.moduleextraction.filters.SupportedExpressivenessFilter;
 import uk.ac.liv.ontologyutils.axioms.AxiomStructureInspector;
+import uk.ac.liv.ontologyutils.ontologies.OntologyCycleVerifier;
 
 import java.util.Set;
 
@@ -27,7 +28,13 @@ public class STARAMEXHybridExtractor extends GenericHybridExtractor{
 
     @Override
     Set<OWLLogicalAxiom> extractUsingSecondExtractor(Set<OWLEntity> signature) {
-        return null;
+        Set<OWLLogicalAxiom> unsupported = getUnsupportedAxioms(module);
+        module.removeAll(unsupported);
+        Set<OWLLogicalAxiom> cycleCausing = getCycleCausingAxioms(module);
+        module.removeAll(cycleCausing);
+        unsupported.addAll(cycleCausing);
+
+        return new AMEX(module).extractModule(unsupported,signature);
     }
 
     private Set<OWLLogicalAxiom> getUnsupportedAxioms(Set<OWLLogicalAxiom> axioms){
@@ -36,10 +43,12 @@ public class STARAMEXHybridExtractor extends GenericHybridExtractor{
         filters.addFilter(new SupportedExpressivenessFilter());
         filters.addFilter(new SharedNameFilter(inspector));
         filters.addFilter(new RepeatedEqualitiesFilter(inspector));
-
-        //Remove cycles
-
         return filters.getUnsupportedAxioms(axioms);
+    }
+
+    private Set<OWLLogicalAxiom> getCycleCausingAxioms(Set<OWLLogicalAxiom> axioms){
+        OntologyCycleVerifier cycleVerifier = new OntologyCycleVerifier(axioms);
+        return cycleVerifier.getCycleCausingAxioms();
     }
 
 
